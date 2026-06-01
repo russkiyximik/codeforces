5e26a1b42bc0c33802d5c60f8db6a9c2f80ba42b
russkiyximik <alex.ponomar37@gmail.com>
2026-06-01 17:47:13 -0500

    finished q3 (brute force)

--- div2/rd1101/3.cpp
+++ div2/rd1101/3.cpp
@@ -1,58 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// TODO: REWRITE USING GREEDY ONCE I LEARN GREEDY
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+ll s, t;
+
+ll eval(string& S, int m) {
+    ll T = 0, ans = 0;
+
+    for (char c : S) {
+        if (c == 'I') {
+            if (T < t) {
+                T++;
+                ans++;
+            }
+        }
+        else if (c == 'E') {
+            if (ans < T * s) ans++;
+        }
+        else {
+            if (m-->0) {
+                if (T < t) {
+                    T++;
+                    ans++;
+                }
+            }
+            else {
+                if (ans < T * s) ans++;
+            }
+        }
+    }
+    
+    return ans;
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int t;
-    cin >> t;
-
-    while (t--) {
-        int n, x, s;
-        cin >> n >> x >> s;
-        vector<string> tables(x);
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n >> t >> s;
         
-        string mystr;
-        cin >> mystr;
-        int i = 0;
-        int ans = 0;
-        for (char c : mystr) {
-            if (c == 'I') {
-                while (i < x) {
-                    if (tables[i] == "") {
-                        tables[i] += c;
-                        ans++;
-                        break;
-                    }
-                    i++;
-                }
-            }
-            else if (c == 'E') {
-                while (i < x) {
-                    if (!tables[i].empty() && tables[i].size() < s) {
-                        tables[i] += c;
-                        ans++;
-                        break;
-                    }
-                    i++;
-                }
-            }
-            else {
-                while (i < x) {
-                    if (tables[i].size() < s) {
-                        tables[i] += c;
-                        ans++;
-                        break;
-                    }
-                    i++;
-                }
-            }
+        string S;
+        cin >> S;
 
-            i=0;
-        }
+        int l = 0, r = 0;
+        for (char c : S) if (c=='A') r++;
+
+        ll ans = 0;
+        for (int i = l; i <= r; i++) ans = max(ans, eval(S, i));
 
         cout << ans << endl;
     }
